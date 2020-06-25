da1e163e12c420ae362f80a635ca814ade02b322
lyangHiga <lyang_higa@poli.ufrj.br>
2020-06-25 13:08:31 -0300

    Add Create posts

---
+++ controllers/postController.js
@@ -0,0 +1,26 @@
+const { validationResult } = require('express-validator');
+
+const Profile = require('../models/Profile');
+const User = require('../models/User');
+const Post = require('../models/Post');
+
+exports.createPost = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const user = await User.findById(req.user.id);
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    });
+    const post = await newPost.save();
+    return res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
-// @route   GET api/posts
-// @desc    TEST route
-// access   Public
-router.get('/', (req, res) => {
-  res.send('Posts Route');
-});
+const { auth } = require('../../controllers/authController');
+const { createPost } = require('../../controllers/postController');
+
+router
+  .route('/')
+  // @route    POST api/posts
+  // @desc     Create a post
+  // @access   Private
+  .post(auth, [check('text', 'Text is required').not().isEmpty()], createPost);
 
 module.exports = router;
