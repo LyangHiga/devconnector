2e1c27b836286958e31463531deabde4f128ca68
lyangHiga <lyang_higa@poli.ufrj.br>
2020-07-24 16:34:13 -0300

    Add Single Post Component

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,39 @@
+import React, { useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
+import { getPost } from '../../actions/post';
+import PostItem from '../posts/PostItem';
+
+const Post = (props) => {
+  const {
+    getPost,
+    post: { post, loading },
+    match,
+  } = props;
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost]);
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Back To Posts
+      </Link>
+      <PostItem showActions={false} post={post} />
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -12,53 +12,62 @@ const PostItem = (props) => {
     addLike,
     removeLike,
     deletePost,
+    showActions,
   } = props;
   return (
     <div class='post bg-white p-1 my-1'>
       <div>
         <Link to={`/profile/${user}`}>
-          <img class='round-img' src={avatar} alt='' />
+          <img className='round-img' src={avatar} alt='' />
           <h4>{name}</h4>
         </Link>
       </div>
       <div>
-        <p class='my-1'>{text}</p>
-        <p class='post-date'>
+        <p className='my-1'>{text}</p>
+        <p className='post-date'>
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
         </p>
-        <button
-          type='button'
-          onClick={() => addLike(_id)}
-          class='btn btn-light'
-        >
-          <i class='fas fa-thumbs-up' />{' '}
-          <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
-        </button>
-        <button
-          type='button'
-          onClick={() => removeLike(_id)}
-          class='btn btn-light'
-        >
-          <i class='fas fa-thumbs-down' />
-        </button>
-        <Link to={`/post/${_id}`} class='btn btn-primary'>
-          Discussion{' '}
-          {comments.length > 0 && (
-            <span class='comment-count'>{comments.length}</span>
-          )}
-        </Link>
-        {!auth.loading && user === auth.user._id && (
-          <button
-            type='button'
-            onClick={() => deletePost(_id)}
-            class='btn btn-danger'
-          >
-            <i class='fas fa-times' />
-          </button>
+        {showActions && (
+          <Fragment>
+            <button
+              type='button'
+              onClick={() => addLike(_id)}
+              className='btn btn-light'
+            >
+              <i className='fas fa-thumbs-up' />{' '}
+              <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+            </button>
+            <button
+              type='button'
+              onClick={() => removeLike(_id)}
+              className='btn btn-light'
+            >
+              <i className='fas fa-thumbs-down' />
+            </button>
+            <Link to={`/posts/${_id}`} className='btn btn-primary'>
+              Discussion{' '}
+              {comments.length > 0 && (
+                <span className='comment-count'>{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
+              <button
+                type='button'
+                onClick={() => deletePost(_id)}
+                className='btn btn-danger'
+              >
+                <i className='fas fa-times' />
+              </button>
+            )}
+          </Fragment>
         )}
       </div>
     </div>
   );
+};
+
+PostItem.defaultProps = {
+  showActions: true,
 };
 
 PostItem.propTypes = {
