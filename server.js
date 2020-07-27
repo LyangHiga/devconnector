ca5103afdc6384d42ae8aecdacae904120611326
lyangHiga <lyang_higa@poli.ufrj.br>
2020-07-27 15:13:32 -0300

    Deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const authRouter = require('./routes/api/auth');
 const postsRouter = require('./routes/api/posts');
 const profileRouter = require('./routes/api/profile');
 const usersRouter = require('./routes/api/users');
+const path = require('path');
 
 const app = express();
 
@@ -17,13 +18,21 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Routes
 app.use('/api/auth', authRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/users', usersRouter);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
